90510d4ea3ee099583fcd7e5963a975f4bcd8224
day070 <hidayatsalmanan25@gmail.com>
2023-12-15 23:14:58 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,24 +1,61 @@
-fetch("https://pokeapi.co/api/v2/pokemon")
-  .then(function (response) {
-    if (response.status !== 200) {
-      console.log("Ooops " + response.status);
-      return;
-    }
-    response.json().then(function (data) {
-      const pokemons = data.results;
-      pokemons.forEach((pokemon) => {
-        document
-          .getElementById("pokemon-list")
-          .insertAdjacentHTML(
-            "beforeend",
-            `<li onclick="detail('${pokemon.url}')">${pokemon.name}</li>`
-          );
+let offset = 0;
+
+function fetchPokemonList(offset) {
+  fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`)
+    .then(function (response) {
+      if (response.status !== 200) {
+        console.log("Ooops " + response.status);
+        return;
+      }
+      response.json().then(function (data) {
+        const pokemons = data.results;
+        const pokemonListElement = document.getElementById("pokemon-list");
+
+        pokemons.forEach((pokemon) => {
+          fetch(pokemon.url)
+            .then(function (response) {
+              if (response.status !== 200) {
+                console.log("Ooops " + response.status);
+                return;
+              }
+              return response.json();
+            })
+            .then(function (pokemonData) {
+              const imgSrc =
+                pokemonData.sprites.other.dream_world.front_default;
+
+              const listItem = document.createElement("li");
+              listItem.innerHTML = `
+                <img src="${imgSrc}" alt="${pokemonData.name}" class="pokemon-image"/>
+                <p onclick="detail('${pokemon.url}')">${pokemonData.name}</p>
+              `;
+              pokemonListElement.appendChild(listItem);
+            })
+            .catch(function (err) {
+              console.log(err);
+            });
+        });
       });
+    })
+    .catch(function (err) {
+      console.log(err);
     });
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
+}
+
+// load page
+function loadPage() {
+  offset += 10;
+  document.getElementById("pokemon-list").innerHTML = "";
+  fetchPokemonList(offset);
+}
+
+function prevPage() {
+  offset -= 10;
+  document.getElementById("pokemon-list").innerHTML = "";
+  fetchPokemonList(offset);
+}
+
+fetchPokemonList(offset);
 
 function detail(url) {
   fetch(url)
@@ -35,14 +72,18 @@ function detail(url) {
         .map((move) => move.move.name)
         .join(", ");
       document.getElementById("detail").innerHTML = `
-          <h3>${pokemon.name}</h3>
-          <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+      <div class="pokemon-view">
+      <img src="${pokemon.sprites.other.dream_world.front_default}" class="img-detail">
+      <h2 class="nama-pkm">${pokemon.name}</h2>
           <p>Height: ${pokemon.height}</p>
           <p>Weight: ${pokemon.weight}</p>
           <p>Skills: ${skills}</p>
+          </div>
         `;
     })
     .catch(function (err) {
       console.log(err);
     });
 }
+
+fetchPokemonList(offset);
